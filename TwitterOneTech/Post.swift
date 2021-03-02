3263f21104fdde44beb0307ece61c29e5c369006
Enovaid <180103167@stu.sdu.edu.kz>
2021-03-02 21:44:29 +0600

    first commit

---
+++ TwitterOneTech/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  TwitterOneTech
+//
+//  Created by Айдана on 12/29/20.
+//
+
+import Foundation
---
+++ TwitterOneTech/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  TwitterOneTech
+//
+//  Created by Айдана on 12/29/20.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
